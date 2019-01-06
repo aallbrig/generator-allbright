15077af7a3bf703c06eb42845c46fa4c015313ea
aallbright <aallbrig@gmail.com>
2019-01-06 16:49:24 -0500

    Break up generator into "prompting" and "writing" functionality.

--- generators/git-ignore/index.js
+++ generators/git-ignore/index.js
@@ -1,6 +1,7 @@
 const Generator = require('yeoman-generator');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const curry = require('ramda').curry;
 
 const directory_reader_promise = (target_folder) => new Promise((res, rej) => {
     fs.readdir(target_folder, (err, files) => {
@@ -22,11 +23,15 @@ const ignore_inclusion_question_generator = (choices = []) => ({
     name: 'included_git_ignore',
     choices
 });
-
 const into_array = (prev, current) => ([...prev, current]);
+const into_single_string = (prev, curr) => `${prev}\n${curr}`;
+const prepend_correct_folder_prefix = curry((sourceFiles, targetFiles, mapFn) =>
+    sourceFiles
+        .filter(_ => targetFiles.includes(_))
+        .map(mapFn)
+);
 
 module.exports = class extends Generator {
-
     async prompting() {
         const { log } = this;
 
@@ -34,30 +39,30 @@ module.exports = class extends Generator {
         const ide_folder = this.templatePath('ide');
         const language_folder = this.templatePath('language');
 
-        const [os_files, ide_files, language_files] = await Promise.all(
+        const [ os_files, ide_files, language_files ] = await Promise.all(
             [os_folder, ide_folder, language_folder].map(directory_reader_promise)
         );
 
+        // Using reduce instead of map so inquirer.Separator('section title') won't exist if no files are pulled from directory
         const which_git_ignores_to_include = ignore_inclusion_question_generator([
             ...os_files.reduce(into_array, [new inquirer.Separator(' = OS Specific = ')]),
             ...ide_files.reduce(into_array, [new inquirer.Separator(' = IDE Specific = ')]),
             ...language_files.reduce(into_array, [new inquirer.Separator(' = Language Specific = ')])
         ]);
 
-        const questions = [which_git_ignores_to_include];
-        const { included_git_ignore } = await this.prompt(questions);
+        const questions = [ which_git_ignores_to_include ];
 
-        // TODO: below "filter/map" chain looks like it could be abstracted
-        const os_selections = included_git_ignore
-            .filter(_ => os_files.includes(_))
-            .map(_ => this.templatePath(`os/${_}`));
-        const ide_selections = included_git_ignore
-            .filter(_ => ide_files.includes(_))
-            .map(_ => this.templatePath(`ide/${_}`));
-        const language_selections = included_git_ignore
-            .filter(_ => language_files.includes(_))
-            .map(_ => this.templatePath(`language/${_}`));
-        // /TODO
+        this.files_read = { os_files, ide_files, language_files };
+        this.answers = await this.prompt(questions);
+    }
+
+    async writing() {
+        const { log, files_read: { os_files, ide_files, language_files }, answers: { included_git_ignore }} = this;
+
+        const prepend_fn_loaded_with_response = prepend_correct_folder_prefix(included_git_ignore);
+        const os_selections = prepend_fn_loaded_with_response(os_files, _ => this.templatePath(`os/${_}`));
+        const ide_selections = prepend_fn_loaded_with_response(ide_files, _ => this.templatePath(`ide/${_}`));
+        const language_selections = prepend_fn_loaded_with_response(language_files, _ => this.templatePath(`language/${_}`));
 
         const selection_paths = [
             ...os_selections,
@@ -66,7 +71,7 @@ module.exports = class extends Generator {
         ];
 
         const all_file_contents = await Promise.all(selection_paths.map(file_reader_promise));
-        const single_ignore_file_content = all_file_contents.reduce((prev, curr) => `${prev}\n${curr}`, "");
+        const single_ignore_file_content = all_file_contents.reduce(into_single_string, "");
 
         fs.writeFile(this.destinationPath('.gitignore'), single_ignore_file_content, (err) => {
             if (err) log('file write error...');
